fccb2792a25db87bf4ce4da1adce2def97ef3e64
John Honan <jhonan@gmail.com>
2017-10-18 19:13:16 +0100

    Create daily_mail.py

---
+++ daily_mail.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 23:03:29 2017
+
+@author: jhonan
+"""
+
+import feedparser
+import markovify
+
+d = feedparser.parse('feed://www.dailymail.co.uk/articles.rss')
+text = []
+
+for post in d.entries:
+    text.append(post.title)
+    text.append(post.summary)
+    
+text_model = markovify.Text(text)
+
+for i in range(100):
+    print(text_model.make_short_sentence(140))
